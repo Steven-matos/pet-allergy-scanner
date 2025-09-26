308e3bb6a48cd32700560c2c3df8422b9d010e6a
Steven Matos <steven_matos@ymail.com>
2025-09-26 17:35:40 -0400

    initial commit

---
+++ pet-allergy-scanner/pet-allergy-scanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pet-allergy-scanner
+//
+//  Created by Steven Matos on 9/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pet-allergy-scanner/pet-allergy-scanner/pet_allergy_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pet_allergy_scannerApp.swift
+//  pet-allergy-scanner
+//
+//  Created by Steven Matos on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct pet_allergy_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
