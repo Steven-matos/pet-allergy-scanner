270e52f7a8d18358ff296356b7d48904e7054a61
Steven Matos <steven_matos@ymail.com>
2025-11-29 12:56:43 -0500

    Fix: Match NutritionalTrendsDashboard model to backend response
    - Changed from period_start/period_end to trend_period
    - Added calorie_trends, macronutrient_trends, feeding_patterns arrays
    - Add AnyCodable helper for dynamic JSON (Dict[str, Any])
    - Matches server/app/models/nutrition/advanced_nutrition.py

--- pet-allergy-scanner/SniffTest/Features/Nutrition/Models/NutritionTrendsModels.swift
+++ pet-allergy-scanner/SniffTest/Features/Nutrition/Models/NutritionTrendsModels.swift
@@ -140,26 +140,78 @@ struct NutritionalTrendResponse: Codable {
 /**
  * Nutritional Trends Dashboard
  * Dashboard response from backend API
+ * Matches server/app/models/nutrition/advanced_nutrition.py:NutritionalTrendsDashboard
  */
 struct NutritionalTrendsDashboard: Codable {
     let petId: String
-    let periodStart: Date
-    let periodEnd: Date
-    let totalCalories: Double
-    let averageDailyCalories: Double
-    let averageFeedingFrequency: Double
-    let weightCorrelation: WeightCorrelationData?
+    let trendPeriod: String
+    let calorieTrends: [[String: AnyCodable]]
+    let macronutrientTrends: [[String: AnyCodable]]
+    let weightCorrelation: [String: AnyCodable]
+    let feedingPatterns: [[String: AnyCodable]]
     let insights: [String]
     
     enum CodingKeys: String, CodingKey {
         case petId = "pet_id"
-        case periodStart = "period_start"
-        case periodEnd = "period_end"
-        case totalCalories = "total_calories"
-        case averageDailyCalories = "average_daily_calories"
-        case averageFeedingFrequency = "average_feeding_frequency"
+        case trendPeriod = "trend_period"
+        case calorieTrends = "calorie_trends"
+        case macronutrientTrends = "macronutrient_trends"
         case weightCorrelation = "weight_correlation"
+        case feedingPatterns = "feeding_patterns"
         case insights
+    }
+}
+
+/**
+ * AnyCodable helper for dynamic JSON fields
+ * Used for Dict[str, Any] from Python backend
+ */
+struct AnyCodable: Codable {
+    let value: Any
+    
+    init(_ value: Any) {
+        self.value = value
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let intValue = try? container.decode(Int.self) {
+            value = intValue
+        } else if let doubleValue = try? container.decode(Double.self) {
+            value = doubleValue
+        } else if let stringValue = try? container.decode(String.self) {
+            value = stringValue
+        } else if let boolValue = try? container.decode(Bool.self) {
+            value = boolValue
+        } else if let arrayValue = try? container.decode([AnyCodable].self) {
+            value = arrayValue.map { $0.value }
+        } else if let dictValue = try? container.decode([String: AnyCodable].self) {
+            value = dictValue.mapValues { $0.value }
+        } else {
+            value = NSNull()
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        
+        switch value {
+        case let intValue as Int:
+            try container.encode(intValue)
+        case let doubleValue as Double:
+            try container.encode(doubleValue)
+        case let stringValue as String:
+            try container.encode(stringValue)
+        case let boolValue as Bool:
+            try container.encode(boolValue)
+        case let arrayValue as [Any]:
+            try container.encode(arrayValue.map { AnyCodable($0) })
+        case let dictValue as [String: Any]:
+            try container.encode(dictValue.mapValues { AnyCodable($0) })
+        default:
+            try container.encodeNil()
+        }
     }
 }
 
