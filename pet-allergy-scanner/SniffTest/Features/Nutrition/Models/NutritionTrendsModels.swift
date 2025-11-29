2a8c18ae8dba785d4600364dd81e532240c2fb21
Steven Matos <steven_matos@ymail.com>
2025-11-29 12:58:45 -0500

    Fix: Remove duplicate AnyCodable declaration
    - AnyCodable already exists in FoodProduct.swift
    - Remove duplicate from NutritionTrendsModels.swift
    - Use existing AnyCodable for dashboard model
    - All compiler errors resolved

--- pet-allergy-scanner/SniffTest/Features/Nutrition/Models/NutritionTrendsModels.swift
+++ pet-allergy-scanner/SniffTest/Features/Nutrition/Models/NutritionTrendsModels.swift
@@ -141,6 +141,8 @@ struct NutritionalTrendResponse: Codable {
  * Nutritional Trends Dashboard
  * Dashboard response from backend API
  * Matches server/app/models/nutrition/advanced_nutrition.py:NutritionalTrendsDashboard
+ * 
+ * Note: Uses AnyCodable for dynamic JSON fields (Dict[str, Any] from Python)
  */
 struct NutritionalTrendsDashboard: Codable {
     let petId: String
@@ -159,59 +161,6 @@ struct NutritionalTrendsDashboard: Codable {
         case weightCorrelation = "weight_correlation"
         case feedingPatterns = "feeding_patterns"
         case insights
-    }
-}
-
-/**
- * AnyCodable helper for dynamic JSON fields
- * Used for Dict[str, Any] from Python backend
- */
-struct AnyCodable: Codable {
-    let value: Any
-    
-    init(_ value: Any) {
-        self.value = value
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        
-        if let intValue = try? container.decode(Int.self) {
-            value = intValue
-        } else if let doubleValue = try? container.decode(Double.self) {
-            value = doubleValue
-        } else if let stringValue = try? container.decode(String.self) {
-            value = stringValue
-        } else if let boolValue = try? container.decode(Bool.self) {
-            value = boolValue
-        } else if let arrayValue = try? container.decode([AnyCodable].self) {
-            value = arrayValue.map { $0.value }
-        } else if let dictValue = try? container.decode([String: AnyCodable].self) {
-            value = dictValue.mapValues { $0.value }
-        } else {
-            value = NSNull()
-        }
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        
-        switch value {
-        case let intValue as Int:
-            try container.encode(intValue)
-        case let doubleValue as Double:
-            try container.encode(doubleValue)
-        case let stringValue as String:
-            try container.encode(stringValue)
-        case let boolValue as Bool:
-            try container.encode(boolValue)
-        case let arrayValue as [Any]:
-            try container.encode(arrayValue.map { AnyCodable($0) })
-        case let dictValue as [String: Any]:
-            try container.encode(dictValue.mapValues { AnyCodable($0) })
-        default:
-            try container.encodeNil()
-        }
     }
 }
 
